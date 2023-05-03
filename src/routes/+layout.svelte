dad00817d1b8de8c625faadae4de2e7880fea82b
Xenuku <50945740+Xenuku@users.noreply.github.com>
2023-05-03 17:40:27 +1000

    Add gradient text colour Fix menu on mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,22 +7,22 @@
 
 <nav class="w-full px-8 py-4 bg-slate-500 text-white">
 	<div class="hidden sm:flex justify-between">
-		<h1 class="text-xl">Xenuku</h1>
+		<a href="/" class="text-xl">Xenuku</a>
 		<div>
 			<a class="hover:text-blue-400" href="/">Home</a>
-			<a class="hover:text-blue-400 pl-2" href="/personal">Personal</a>
 			<a class="hover:text-blue-400 pl-2" href="/blog">Blog</a>
+			<a class="hover:text-blue-400 pl-2" href="/personal">Personal</a>
 		</div>
 	</div>
 	<div class="flex justify-between sm:hidden">
-		<h1 class="text-xl p-2">Xenuku</h1>
+		<a href="/" class="text-xl p-2">Xenuku</a>
 		<!-- Hamburger -->
 		<div class="-mr-2 flex items-center sm:hidden">
 			<button
 				on:click={() => (open ? (open = false) : (open = true))}
 				class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out"
 			>
-				<svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+				<svg class="h-6 w-6" stroke="#C4C4C4" fill="none" viewBox="0 0 24 24">
 					<path
 						class={open ? 'hidden' : 'inline-flex'}
 						stroke-linecap="round"
@@ -49,6 +49,6 @@
 		</div>
 	{/if}
 </nav>
-<div class="px-8 bg-gray-800 min-h-screen">
+<div class="px-8 pt-2 bg-gray-800 min-h-screen">
 	<slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,26 @@
 <svelte:head>
 	<title>Xenuku - Home</title>
 </svelte:head>
+
+<div>
+	<h2 class="text-4xl text-center text-[#F8F8F8]">
+		G'day, I'm <span
+			class="bg-gradient-to-r from-emerald-400 to-[#2a98ec] bg-clip-text text-transparent"
+			>Xenuku</span
+		>
+		or
+		<span class="bg-gradient-to-r from-[#2a98ec] to-emerald-400 bg-clip-text text-transparent"
+			>Liam</span
+		>
+	</h2>
+	<!-- <p>
+		My name is Liam. I enjoy building web applications and software. I'm currently a full-time
+		university student, expecting to graduate mid-2023. I first got started with web development by
+		customising forum themes and building small informational websites for myself back in 2010. I
+		found a passion in what I created and since then I have worked as a web developer building with
+		Wordpress, Laravel, and vanilla PHP. I have since moved further into the JavaScript side of the
+		web, where I am enjoying ReactJS and Svelte. I also like exploring game and mobile application
+		programming. I continue to learn both at university and in my free time, in all types of
+		technologies that interest me. In my spare time I love reading Fantasy and Sci-Fi.
+	</p> -->
+</div>
