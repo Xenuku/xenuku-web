09bdf9cb49fd45d53eb049aba36bf18e36b5a97d
Xenuku <50945740+Xenuku@users.noreply.github.com>
2023-05-05 13:59:50 +1000

    Update personal page to use variables

--- src/routes/personal/+page.svelte
+++ src/routes/personal/+page.svelte
@@ -1,6 +1,12 @@
 <script>
 	import Card from '$lib/card.svelte';
 	import Category from '$lib/category.svelte';
+	let watching = 'images/showswatching';
+	let reading = 'images/books';
+	let playing = 'images/gamesplaying';
+	let lovedShows = 'images/lovedshows';
+	let lovedMovies = 'images/lovedmovies';
+	let lovedGames = 'images/lovedgames';
 </script>
 
 <svelte:head>
@@ -10,39 +16,47 @@
 <div>
 	<h2 class="text-4xl text-center text-blue-400">Personal</h2>
 	<Category categoryName="Currently Watching / Plan to Watch">
-		<Card url="images/showswatching/onepiece.png" name="One Piece" />
-		<Card url="images/showswatching/sevendeadlysins.jpg" name="The Seven Deadly Sins" />
-		<Card url="images/showswatching/got.png" name="Game of Thrones" />
-		<Card url="images/showswatching/startrek.png" name="All of Star Trek" />
+		<Card url="{watching}/onepiece.png" name="One Piece" />
+		<Card url="{watching}/sevendeadlysins.jpg" name="The Seven Deadly Sins" />
+		<Card url="{watching}/got.png" name="Game of Thrones" />
+		<Card url="{watching}/startrek.png" name="All of Star Trek" />
 	</Category>
 	<Category categoryName="Currently Reading / Plan to Read">
-		<Card url="images/books/dcc.jpg" name="Dungeon Crawler Carl #2" />
-		<Card url="images/books/hwfwm.jpg" name="He Who Fights With Monsters" />
-		<Card url="images/books/mistborn.jpg" name="Mistborn" />
-		<Card url="images/books/leviathanwakes.jpg" name="Leviathan Wakes" />
-		<Card url="images/books/theprimalhunter.jpg" name="The Primal Hunter #6" />
+		<Card url="{reading}/dcc.jpg" name="Dungeon Crawler Carl #2" />
+		<Card url="{reading}/hwfwm.jpg" name="He Who Fights With Monsters" />
+		<Card url="{reading}/mistborn.jpg" name="Mistborn" />
+		<Card url="{reading}/leviathanwakes.jpg" name="Leviathan Wakes" />
+		<Card url="{reading}/theprimalhunter.jpg" name="The Primal Hunter #6" />
 	</Category>
 	<Category categoryName="Currently Playing / Plan to Play">
-		<Card url="images/gamesplaying/osrs.jpg" name="OSRS" />
-		<Card url="images/gamesplaying/runescape.jpg" name="Runescape" />
-		<Card url="images/gamesplaying/hitman3.jpg" name="Hitman 3" />
-		<Card url="images/gamesplaying/civ6.jpeg" name="Civilization VI" />
-		<Card url="images/gamesplaying/tome.jpg" name="Tales of Maj'Eyal" />
+		<Card url="{playing}/osrs.jpg" name="OSRS" />
+		<Card url="{playing}/runescape.jpg" name="Runescape" />
+		<Card url="{playing}/hitman3.jpg" name="Hitman 3" />
+		<Card url="{playing}/civ6.jpeg" name="Civilization VI" />
+		<Card url="{playing}/tome.jpg" name="Tales of Maj'Eyal" />
 	</Category>
-	<Category categoryName="Loved Shows">
-		<Card url="images/lovedshows/stargate.jpg" name="Stargate" />
-		<Card url="images/lovedshows/dark.png" name="DARK" />
-		<Card url="images/lovedshows/dragonball.png" name="Dragon Ball" />
-		<Card url="images/lovedshows/invincible.png" name="Invincible" />
-		<Card url="images/lovedshows/farscape.jpg" name="Farscape" />
-		<Card url="images/lovedshows/theoffice.jpg" name="The Office" />
+	<Category categoryName="Favourite Games">
+		<Card url="{lovedGames}/drg.jpg" name="Deep Rock Galactic" />
+		<Card url="{lovedGames}/animalcrossing.jpg" name="Animal Crossing" />
+		<Card url="{lovedGames}/banjokazooie.jpg" name="Banjo Kazooie" />
+		<Card url="{lovedGames}/mhw.jpg" name="Monster Hunter: World" />
+		<Card url="{lovedGames}/p5r.png" name="Persona 5 Royal" />
+		<Card url="{lovedGames}/supraland.jpg" name="Supraland" />
 	</Category>
-	<Category categoryName="Loved Movies">
-		<Card url="images/lovedmovies/gunsakimbo.jpg" name="Guns Akimbo" />
-		<Card url="images/lovedmovies/httyd.jpg" name="HTTYD" />
-		<Card url="images/lovedmovies/klaus.jpg" name="Klaus" />
-		<Card url="images/lovedmovies/scottpilgrim.jpg" name="Scott Pilgrim" />
-		<Card url="images/lovedmovies/spaceballs.jpg" name="Spaceballs" />
-		<Card url="images/lovedmovies/spiderverse.png" name="Into the Spider-Verse" />
+	<Category categoryName="Favourite Shows">
+		<Card url="{lovedShows}/stargate.jpg" name="Stargate" />
+		<Card url="{lovedShows}/dark.png" name="DARK" />
+		<Card url="{lovedShows}/dragonball.png" name="Dragon Ball" />
+		<Card url="{lovedShows}/invincible.png" name="Invincible" />
+		<Card url="{lovedShows}/farscape.jpg" name="Farscape" />
+		<Card url="{lovedShows}/theoffice.jpg" name="The Office" />
+	</Category>
+	<Category categoryName="Favourite Movies">
+		<Card url="{lovedMovies}/gunsakimbo.jpg" name="Guns Akimbo" />
+		<Card url="{lovedMovies}/httyd.jpg" name="HTTYD" />
+		<Card url="{lovedMovies}/klaus.jpg" name="Klaus" />
+		<Card url="{lovedMovies}/scottpilgrim.jpg" name="Scott Pilgrim" />
+		<Card url="{lovedMovies}/spaceballs.jpg" name="Spaceballs" />
+		<Card url="{lovedMovies}/spiderverse.png" name="Into the Spider-Verse" />
 	</Category>
 </div>
